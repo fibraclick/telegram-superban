8b785a2de79861c528724a5412ef02ee3f13665c
Francesco Tonini <francesco@tonini.dev>
2022-06-22 22:29:37 +0200

    Fix deploy directory

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -4,7 +4,7 @@ set -e
 # Log into AWS ECR
 echo $DOCKER_REGISTRY_TOKEN | docker login --username AWS --password-stdin $DOCKER_REGISTRY_NAME
 
-cd /home/deploy/telegram-superban
+cd /home/deploy/fibraclick-telegram-superban
 echo DOCKER_TAG=$DOCKER_TAG > .env
 echo HOSTNAME=$HOSTNAME >> .env
 docker-compose -f docker-compose.prod.yml pull
